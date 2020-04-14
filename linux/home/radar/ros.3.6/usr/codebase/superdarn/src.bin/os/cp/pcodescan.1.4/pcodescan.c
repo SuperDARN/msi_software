133366b4dcd5a2ef31db508427cab2da192713f0
ksterne <kevintyler@vt.edu>
2020-04-14 15:44:30 +0000

    Adding iqwrite back in

--- linux/home/radar/ros.3.6/usr/codebase/superdarn/src.bin/os/cp/pcodescan.1.4/pcodescan.c
+++ linux/home/radar/ros.3.6/usr/codebase/superdarn/src.bin/os/cp/pcodescan.1.4/pcodescan.c
@@ -71,11 +71,12 @@ struct TCPIPMsgHost errlog={"127.0.0.1",44100,-1};
 
 struct TCPIPMsgHost shell={"127.0.0.1",44101,-1};
 
-int tnum=3;
-struct TCPIPMsgHost task[3]={
-  {"127.0.0.1",2,-1}, /* raw acfwrite */
-  {"127.0.0.1",3,-1}, /* fit acf write */
-  {"127.0.0.1",4,-1} /* rt server */
+int tnum=4;
+struct TCPIPMsgHost task[4]={
+  {"127.0.0.1",1,-1}, /* iqwrite */
+  {"127.0.0.1",2,-1}, /* rawacfwrite */
+  {"127.0.0.1",3,-1}, /* fitacfwrite */
+  {"127.0.0.1",4,-1} /* rtserver */
 };
 
 int main(int argc,char *argv[]) {
