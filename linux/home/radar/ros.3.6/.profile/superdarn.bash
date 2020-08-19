d68fffb2420cec38cb1cdbfbf7d898c7f78efb92
ksterne <kevintyler@vt.edu>
2020-08-19 02:35:52 +0000

    Typo on SND path

--- linux/home/radar/ros.3.6/.profile/superdarn.bash
+++ linux/home/radar/ros.3.6/.profile/superdarn.bash
@@ -21,7 +21,7 @@ export SD_ERRLOG_PATH="/data/ros/errlog";
 export SD_FIT_PATH="/data/ros/fitacf"
 export SD_IQ_PATH="/data/ros/iqdat"
 export SD_RAW_PATH="/data/ros/rawacf"
-export SD_SND_PATH='/data/ros/snd"
+export SD_SND_PATH="/data/ros/snd"
 
 
 
